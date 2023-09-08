cc5613ad0b293712cc7305515aeba2d61a4dc365
barbara <barbara.ayres.pro@gmail.com>
2023-09-08 13:27:19 +0200

    improved score graph css

--- src/components/recharts/ShapePieChart.js
+++ src/components/recharts/ShapePieChart.js
@@ -1,66 +1,80 @@
-import React, { PureComponent } from 'react';
-import { RadialBarChart, RadialBar, ResponsiveContainer, PolarAngleAxis, Area } from 'recharts';
+import React from 'react';
+import { RadialBarChart, RadialBar, PolarAngleAxis } from 'recharts';
 import PropTypes from 'prop-types';
 
+function ShapePieChart({ score }) {
+  const data = [{ name: 'L1', value: score * 100 }];
 
-
-/**
- * This function shows the user's score (in percentage) on a graphic
- * @param {number} score Score in decimal number
- * @return {chart}       Chart with the final result in percentage
- */
-function ShapePieChart({score}) {
- 
-  const data = [
-    { name: 'L1', value: score * 100 }
-  ];
-  
   const circleSize = 260;
+  const barSize = 20; 
 
-
-    return (
-      <ResponsiveContainer width="100%" height="100%">
-        <RadialBarChart
-          width={circleSize}
-          height={circleSize}
-          cx={circleSize / 2}
-          cy={circleSize / 2}
-          innerRadius={100}
-          outerRadius={100}
-          barSize={12}
-          data={data}
-          startAngle={-130}
-          endAngle={-490}
-          >
-          <PolarAngleAxis
+  return (
+    <div style={{ position: 'relative', width: circleSize, height: circleSize }}>
+      <RadialBarChart
+        width={circleSize}
+        height={circleSize}
+        cx={circleSize / 2}
+        cy={circleSize / 2}
+        innerRadius={100}
+        outerRadius={100}
+        barSize={barSize} 
+        data={data}
+        startAngle={-130}
+        endAngle={50} 
+      >
+        <PolarAngleAxis
           type="number"
           domain={[0, 100]}
           angleAxisId={0}
           tick={false}
-          />
-          <Area stroke="#8884d8" fill='#FFFFFF' />
-          <RadialBar
+        />
+        <RadialBar
           clockWise
           dataKey="value"
-          cornerRadius={circleSize / 2}
+          cornerRadius={5} 
           fill="#E60000"
-          />
-          <text
+        />
+        <circle
+          cx={circleSize / 2}
+          cy={circleSize / 2}
+          r={100}
+          fill="#FFFFFF"
+          style={{ borderRadius: '5px' }} 
+        /> 
+        <text
           x={circleSize / 2}
           y={circleSize / 2}
           textAnchor="middle"
           dominantBaseline="middle"
           className="progress-label"
-          >
-          {score * 100}% de votre objectif
-          </text>
-        </RadialBarChart>
-      </ResponsiveContainer>
-    );
-  }
+        >
+          <tspan x={circleSize / 2} dy={-20} textAnchor="middle" fontWeight="bold" fontSize="22px">
+            {`${score * 100}%`}
+          </tspan>
+          <tspan x={circleSize / 2} dy={26} textAnchor="middle" fill="#888">
+            de votre
+          </tspan>
+          <tspan x={circleSize / 2} dy={26} textAnchor="middle" fill="#888">
+            objectif
+          </tspan>
+        </text>
+      </RadialBarChart>
+      <div
+        style={{
+          position: 'absolute',
+          top: '20px',
+          left: '20px',
+          fontSize: '16px',
+        }}
+      >
+        Score
+      </div>
+    </div>
+  );
+}
 
-  ShapePieChart.propTypes = {
-    score: PropTypes.number
-  }
+ShapePieChart.propTypes = {
+  score: PropTypes.number,
+};
 
-  export default ShapePieChart;
+export default ShapePieChart;
